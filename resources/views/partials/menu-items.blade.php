a66dbb54790e38118b6392b521e8eb220a73008c
rahul_cordiace <rahul.k@cordiace.com>
2021-09-25 18:32:00 +0530

    MOVE SIDE NAV ITEMS TO SEPARATE BLADE

---
+++ resources/views/partials/menu-items.blade.php
@@ -0,0 +1,11 @@
+<x-bdskeleton-sidebar-item route="admin.home" prefix="admin" icon="speedometer2" label="Home" />
+<x-bdskeleton-sidebar-item route="admin.home" prefix="admin/user" icon="people" label="Users" />
+
+@php
+    $membershipSubmenus = [
+        'admin.home' => 'View All',
+        'admin.home' => 'Add New'
+    ]
+@endphp
+<x-bdskeleton-sidebar-item route="admin.home" prefix="admin/membership-plan" icon="cash" 
+    label="Membership Plans" :subMenus="$membershipSubmenus" />
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,42 +7,13 @@
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
-        <x-bdskeleton-sidebar-item route="admin.home" prefix="admin" icon="speedometer2" label="Home" />
-        <x-bdskeleton-sidebar-item route="admin.home" prefix="admin/user" icon="people" label="Users" />
-
-        {{-- @php
-            $membershipSubmenus = [
-                'admin.membership-plan.index' => 'View All',
-                'admin.membership-plan.create' => 'Add New'
-            ]
-        @endphp
-        <x-bdskeleton-sidebar-item route="admin.membership-plan.index" prefix="admin/membership-plan" icon="cash" 
-            label="Membership Plans" :subMenus="$membershipSubmenus" /> --}}
-        
-        {{-- @php
-            $clubSubMenus = [
-                'admin.club.index' => 'View All',
-                'admin.club.manage' => 'Add New',
-                'admin.club.designations' => 'Designations'
-            ];
-        @endphp            
-        <x-bdskeleton-sidebar-item route="admin.club.index" prefix="admin/club" icon="mic-fill" label="Club" 
-            :subMenus="$clubSubMenus" />
-
-        @php
-            $awardSubMenus = [
-                'admin.award.index' => 'View All',
-                'admin.award.create' => 'Add New',
-            ];
-        @endphp
-        <x-bdskeleton-sidebar-item route="admin.award.index" prefix="admin/award" icon="award" label="Awards" 
-            :subMenus="$awardSubMenus" /> --}}
+        @include('bootstrap-dashboard-skeleton::partials.menu-items')
     </ul>
     <hr>
     <div class="dropdown">
         <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1"
             data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="{{ asset('images/avatar.png') }}" alt="" width="32" height="32" class="rounded-circle me-2">
+            <img src="{{ asset('vendor/bootstrap-dashboard-skeleton/images/avatar.png') }}" alt="" width="32" height="32" class="rounded-circle me-2">
             <strong>{{ Auth::user()->name }}</strong>
         </a>
         <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
