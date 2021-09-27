1005b633cdb3160525cbde366b5f64ccb1655788
rahul_cordiace <rahul.k@cordiace.com>
2021-09-27 17:30:11 +0530

    ADD RESPONSE TO TAKE ADMIN TO /admin ONCE LOGGED IN

---
+++ src/Http/Responses/LoginResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace IamRahul1973\Skeleton\Http\Responses;
+
+use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Http\Responses\LoginResponse as ResponsesLoginResponseContract;
+
+class LoginResponse extends ResponsesLoginResponseContract
+{
+    public function toResponse($request)
+    {
+        $redirectTo = Auth::user()->hasRole('admin') ? '/admin' : config('fortify.home');
+        return $request->wantsJson()
+            ? response()->json(['two_factor' => false])
+            : redirect()->intended($redirectTo);
+    }
+}
