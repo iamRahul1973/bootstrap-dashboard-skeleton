1d85c505d6cdbb81ed03c2ad91b9274d75a89e92
rahul_cordiace <rahul.k@cordiace.com>
2021-10-15 10:43:50 +0530

    FIX RADIO BUTTON DEFAULT VALUE

--- resources/views/components/form-radio.blade.php
+++ resources/views/components/form-radio.blade.php
@@ -1,11 +1,15 @@
 <div class="col-md-{{ $col }} form-group">
     <label for="{{ $id }}" class="form-label">{{ $label }}</label>
-    <div id="{{ $id }}">
+    <div id="{{ $id }}" class="@error($name) is-invalid @enderror">
         @foreach ($options as $key => $option)
             <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" name="{{ $name }}" 
                     id="inlineRadio_{{ $loop->index + 1 }}" value="{{ $key }}"
-                    @if (boolval($defaultChecked) === boolval($key)) checked @endif>
+                    @if (old($name) === (string) $key)
+                        checked
+                    @elseif((old($name) === null) && ($defaultChecked === (String) $key))
+                        checked
+                    @endif />
                 <label class="form-check-label" for="inlineRadio_{{ $loop->index + 1 }}">{{ $option }}</label>
             </div>
         @endforeach
