913b50a6b045de67ea8a07316a60113533383109
rahul_cordiace <rahul.k@cordiace.com>
2021-10-28 18:45:52 +0530

    TELL THE SPINNER TO SHOW ONLY THE ICON NOT TXT

--- resources/views/components/spinner.blade.php
+++ resources/views/components/spinner.blade.php
@@ -1,6 +1,8 @@
+@props(['withMessage' => true])
+
 <div class="loading">
     <span class="spinner-border" role="status">
         <span class="visually-hidden">Loading...</span>
     </span>
-    Wait a Second ...
+    @if (filter_var($withMessage, FILTER_VALIDATE_BOOLEAN)) Wait a Second ... @endif
 </div>
